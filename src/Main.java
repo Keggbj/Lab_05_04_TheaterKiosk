0f1b43faeb80ed73ed9c5d8f6e89f8b3107e5db2
minik <keggbj@mail.uc.edu>
2022-09-22 16:12:43 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 22;
+
+        if (age >= 21) {
+            System.out.println("You get a wrist band!");
+        }
+    }
+}
